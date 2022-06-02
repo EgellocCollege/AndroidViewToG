dd72533acfe4e8513fe743026f0205a11179ae55
wirelessqa <wirelessqa.me@gmail.com>
2022-06-02 09:01:05 +0800

    Initial commit

---
+++ app/src/main/java/com/self/viewtoglrendering/MainActivity.java
@@ -0,0 +1,45 @@
+package com.self.viewtoglrendering;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.self.viewtoglrendering.cuberenerer.CubeGLRenderer;
+
+
+public class MainActivity extends Activity {
+
+    private GLSurfaceView mGLSurfaceView;
+    private GLRenderable mGLLinearLayout;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initViews();
+    }
+
+    private void initViews() {
+        setContentView(R.layout.activity_main);
+
+        ViewToGLRenderer viewToGlRenderer = new CubeGLRenderer(this);
+
+        mGLSurfaceView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
+        mGLLinearLayout = (GLRenderable) findViewById(R.id.gl_layout);
+        mWebView = (WebView) findViewById(R.id.web_view);
+
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        mGLSurfaceView.setRenderer(viewToGlRenderer);
+
+        mGLLinearLayout.setViewToGLRenderer(viewToGlRenderer);
+
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.loadUrl("http://stackoverflow.com/questions/12499396/is-it-possible-to-render-an-android-view-to-an-opengl-fbo-or-texture");
+    }
+
+
+}
